0604de83a433dc4563798831ffe6c7ef7badd887
epenicela <eltonpenicela@gmail.com>
2023-02-26 01:15:48 +0200

    Final

--- grpc-euromil/client/client-grpc.js
+++
@@ -1,8 +0,0 @@
-const grpc = require('grpc')
-//const PROTO_PATH = '../notes.proto'
-const EuroMil = grpc.load('../euromil.proto').EuroMil
-
-const client = new EuroMil('localhost:50051',
-    grpc.credentials.createInsecure())
-    
-module.exports = client
--- grpc-euromil/client/get_apotas.js
+++
@@ -1,9 +0,0 @@
-const client = require('./client-grpc')
-
-client.list({}, (error, apostas) => {
-    if (!error) {
-        console.log(apostas)
-    } else {
-        console.error(error)
-    }
-})
